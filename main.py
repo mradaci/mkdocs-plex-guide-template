8abcd86492b1c82d5f0729543e6ae519345a83b9
mradaci <icadarmsr@gmail.com>
2025-02-16 17:43:18 -0500

    Updated to use specific Plex server details

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import os
 from datetime import datetime
 
+
 def define_env(env):
     """
     This hook is called when the config is loading.
     """
-    username = os.getenv('GITHUB_REPOSITORY_OWNER', 'Ghost')
-    repo_name = os.getenv('GITHUB_REPOSITORY', 'mkdocs-plex-guide-template').split('/')[-1]
+    username = "ICADAR"
+    repo_name = os.getenv('GITHUB_REPOSITORY',
+                          'mkdocs-plex-guide-template').split('/')[-1]
     current_year = datetime.now().year
 
     # Explicitly check for missing or empty values and assign defaults
-    request_url = os.getenv('REQUEST_URL') or "request.example.com"
-    plex_url = os.getenv('PLEX_URL') or "plex.example.com"
+    request_url = "https://mattflixrequests.icadar.com/"
+    plex_url = "http://192.168.1.100:32400"
     plex_libraries = os.getenv('PLEX_LIBRARIES') or "Movies and TV Shows"
     noreply_email = os.getenv('NOREPLY_EMAIL') or "noreply@example.com"
 
